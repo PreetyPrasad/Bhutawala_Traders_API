d6c6aff3106f33c2f496b2bd89b3bdaeb22e323a
Patel-Zainab <cricketermiss@gmail.com>
2025-01-31 15:12:43 +0530

    insert_category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Bhutawala_Traders_API.ApplicationContext;
+using Bhutawala_Traders_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bhutawala_Traders_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        [HttpPost]
+        [Route("InsertCategory")]
+        public async Task<IActionResult> AddCategory(Category category)
+        {
+            try
+            {
+                if (!_dbContext.Categories.Any(o => o.CategoryId == category.CategoryId))
+                {
+                    _dbContext.Categories.Add(category);
+                    await _dbContext.SaveChangesAsync();
+                    return Ok(new { Status = "Ok", Result = "Successfully Saved" });
+                }
+                else
+                {
+                    return Ok(new { Status = "Fail", Result = "Already Exists" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Status = "Fail", Result = ex.Message });
+            }
+        }
+    }
+}
