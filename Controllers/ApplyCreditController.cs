d5c24fc8b6e7bce89612b8ed2d6a86a01f04f382
prasadpreeti438@gmail.com <prasadpreeti438@gmail.com>
2025-01-31 15:23:46 +0530

    apply_credit

---
+++ Controllers/ApplyCreditController.cs
@@ -0,0 +1,40 @@
+﻿using Bhutawala_Traders_API.ApplicationContext;
+using Bhutawala_Traders_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bhutawala_Traders_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplyCreditController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public ApplyCreditController(ApplicationDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        [HttpPost]
+        [Route("InsertApplyCredit")]
+        public async Task<IActionResult> AddApplyCredit(ApplyCredit applyCredit)
+        {
+            try
+            {
+                if (!_dbContext.ApplyCredits.Any(o => o.ApplyId == ApplyCredit.Appl))
+                {
+                    _dbContext.ApplyCredits.Add(applyCredit);
+                    await _dbContext.SaveChangesAsync();
+                    return Ok(new { Status = "Ok", Result = "Successfully Saved" });
+                }
+                else
+                {
+                    return Ok(new { Status = "Fail", Result = "Already Exists" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Status = "Fail", Result = ex.Message });
+            }
+        }
+    }
+}
---
+++ Controllers/CreditNoteController.cs
@@ -0,0 +1,41 @@
+﻿using Bhutawala_Traders_API.ApplicationContext;
+using Bhutawala_Traders_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bhutawala_Traders_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditNoteController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CreditNoteController(ApplicationDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+        [HttpPost]
+        [Route("InsertCreditNote")]
+        public async Task<IActionResult> AddCreditNote(CreditNote creditNote)
+        {
+            try
+            {
+                if (!_dbContext.CreditNotes.Any(o => o.CreditNoteId == creditNote.CreditNoteId))
+                {
+                    _dbContext.CreditNotes.Add(creditNote);
+                    await _dbContext.SaveChangesAsync();
+                    return Ok(new { Status = "Ok", Result = "Successfully Saved" });
+                }
+                else
+                {
+                    return Ok(new { Status = "Fail", Result = "Already Exists" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Status = "Fail", Result = ex.Message });
+            }
+        }
+
+    }
+}
