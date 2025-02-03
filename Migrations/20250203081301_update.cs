04be90ec3229f4cd1471753e71ef73c98f61a158
Patel-Zainab <cricketermiss@gmail.com>
2025-02-03 13:43:44 +0530

    migration updated

---
+++ Migrations/20250203081301_update.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bhutawala_Traders_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InvoiceId",
+                table: "InvoiceDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "InvoiceMasterInvoiceId",
+                table: "InvoiceDetails",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceDetails_InvoiceMasterInvoiceId",
+                table: "InvoiceDetails",
+                column: "InvoiceMasterInvoiceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceDetails_InvoiceMasters_InvoiceMasterInvoiceId",
+                table: "InvoiceDetails",
+                column: "InvoiceMasterInvoiceId",
+                principalTable: "InvoiceMasters",
+                principalColumn: "InvoiceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceDetails_InvoiceMasters_InvoiceMasterInvoiceId",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoiceDetails_InvoiceMasterInvoiceId",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceId",
+                table: "InvoiceDetails");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceMasterInvoiceId",
+                table: "InvoiceDetails");
+        }
+    }
+}
